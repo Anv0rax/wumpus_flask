752256cc9ab678897e66b9638b0be5f516246611
Nolan <nolanzerrkam@gmail.com>
2026-02-10 01:17:00 +0100

    The settings menu working on the settings page only for now

--- js/select-difficulty.js
+++ js/select-difficulty.js
@@ -1,12 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
+// TODO : Essayer de faire en sorte que si on a activé le param FullScreen, Mute ou autre dans le menu Settings, que ces changements soient aussi effectués partout.
+document.addEventListener('DOMContentLoaded', () =>
+{
+    /* ======== Constantes ========*/
     const choice = document.getElementById('difficulty-choice');
-    const form = document.getElementById('form')
+    const form = document.getElementById('form');
     const move = document.getElementById('son1');
     const interact = document.getElementById('son2');
     const onoff = document.querySelectorAll('.check');
-    const test = document.getElementById('test');
     const validsound = document.getElementById('son3');
-    const mute = document.getElementById('sound');
+
+    const lum = document.getElementById('light');
+    const sfx = document.getElementById('sfx');
+    const music = document.getElementById('music');
+    const sound = document.getElementById('sound');
+
+    const checkFs = document.getElementById('checkFs');
+    const checkPerf = document.getElementById('checkPerf');
+
+    /* ====================================== */
 
     choice.addEventListener('change', (el) => {
         const val = choice.value;
@@ -31,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 1000);
     });
 
-    sound.addEventListener('click', () => {
+        sound.addEventListener('click', () => {
         if(sound.src.includes("SoundWhite.png"))
         {
             sound.src = "../assets/SoundMuteWhite.png";
@@ -42,16 +53,62 @@ document.addEventListener('DOMContentLoaded', () => {
         else
         {
             sound.src = "../assets/SoundWhite.png";
-            move.volume = 1;
-            interact.volume = 1;
-            validsound.volume = 1;
+            move.volume = 0.5;
+            interact.volume = 0.5;
+            validsound.volume = 0.5;
         }
-    });
 
+        /*checkPerf.addEventListener('click', (event) => 
+        {
+            const testCheck = event.target.checked;
+            if(testCheck == true)
+            {
+                lowPerf.style.setProperty('display', 'none', 'important');
+            }
+            else
+            {
+                lowPerf.style.setProperty('display', 'block', 'important');
+            }
+        });
+
+        checkFs.addEventListener('click', (event) => 
+        {
+            const testCheck = event.target.checked;
+            if(testCheck == true)
+            {
+                OpenFs();
+            }
+            else
+            {
+                CloseFs();
+            }
+        });*/
+    });
 });
 
 function PlaySound(el)
 {
     el.currentTime = 0;
     el.play();
+}
+
+function OpenFs()
+{
+    if(document.body)
+    {
+        document.body.requestFullscreen();
+    }
+    /* sa c'est pour safari */
+    else if(document.body.webkitRequestFullscreen)
+    {
+        document.body.webkitRequestFullscreen();
+    }
+}
+
+function CloseFs()
+{
+    if(document.body)
+    {
+        document.exitFullscreen();
+    }
 }
