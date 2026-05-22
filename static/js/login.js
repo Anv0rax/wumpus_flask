ee5f567c3727abed3557cc797cd0e1cf6178eb69
Anv0rax <anvorax@gmail.com>
2026-05-22 20:39:37 +0200

    END

--- static/js/login.js
+++ static/js/login.js
@@ -1,20 +1,41 @@
-document.addEventListener('DOMContentLoaded', function()
-{
+document.addEventListener('DOMContentLoaded', function() {
     const inputUserName = document.getElementById("username");
     const inputPswrd = document.getElementById("password");
-    
-    let regexUserName = /^[a-zA-Z0-9_]{3,10}$/
-    let regexPswrd = /^.{10,30}$/u;
-    
-    const loginForm = document.getElementById('login-form');
-    loginForm.addEventListener('submit', function(event)
-    {
-        let testUsrname = inputUserName.value.trim();
-        let testMdp = inputPswrd.value.trim();
 
-        if(!regexUserName.test(testUsrname) || !regexPswrd.test(testMdp))
+    let regexUserName = /^[a-zA-Z0-9_]{3,10}$/;
+    let regexPswrd = /^.{10,30}$/u;
+
+    inputUserName.addEventListener('keyup', e => { 
+        checkValidity(inputUserName, regexUserName);
+    });
+
+    inputPswrd.addEventListener('keyup', e => { 
+        checkValidity(inputPswrd, regexPswrd);
+    });
+
+    const loginForm = document.getElementById('login-form');
+    
+    loginForm.addEventListener('submit', function(event) {
+        if(!checkValidity(inputUserName, regexUserName) 
+            || !checkValidity(inputPswrd, regexPswrd))
         {
             event.preventDefault();
         }
     });
 });
+
+function checkValidity(input, regex) {
+    var tested = regex.test(input.value.trim());
+    var redColor = "#FF5A3D";
+    var greenColor = "#3DCC6A";
+
+    if(!tested) {
+        input.style.color = redColor;
+        input.style.borderColor = redColor;
+    }
+    else {
+        input.style.color = greenColor;
+        input.style.borderColor = greenColor;
+    }
+    return tested;
+}
